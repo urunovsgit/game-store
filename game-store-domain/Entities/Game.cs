ba0950782090b68a3cf48745bddd2036fe0421e9
urunovs <urunovsbox@gmail.com>
2023-02-09 11:00:53 +0500

    Add domain project and apply bootstrap template

---
+++ game-store-domain/Entities/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_store_domain.Entities
+{
+    public enum GameGenres
+    {
+        Other,
+        Strategy,
+        RPG,
+        Sports,
+        Races,
+        Action,
+        Adventure,
+        Puzzle_and_skill
+    }
+
+    internal class Game
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get;set; }
+
+    }
+}
