bf7c91b890df47ed13daba112bd601ead4989ff1
urunovs <urunovsbox@gmail.com>
2023-03-04 17:35:39 +0500

    Fix filtration by genres

--- game-store/Controllers/HomeController.cs
+++ game-store/Controllers/HomeController.cs
@@ -23,22 +23,25 @@ namespace game_store.Controllers
 
         public IActionResult Index(string titleKey = "")
         {
-            ViewBag.SelectedGenres = Enum.GetValues(typeof(Genre)).OfType<Genre>().ToList();
+            
             ViewBag.GenreNodes = _gameServicesProvider.GetAllGenreNodes();
             List<Game> games;
 
             if(!string.IsNullOrEmpty(titleKey)) 
             {
                 games = _gameServicesProvider.GetGamesByTitle(titleKey).ToList();
+                ViewBag.SelectedGenres = Enum.GetValues(typeof(Genre)).OfType<Genre>().ToList();
             }
-            else if (!TempData.ContainsKey("SelectedGenres"))
-            {
-                games = _gameServicesProvider.GetAllGames().ToList();
-            }
-            else
+            else if (TempData.ContainsKey("SelectedGenres"))
             {
                 var genres = JsonConvert.DeserializeObject<List<Genre>>((string)TempData["SelectedGenres"]);
                 games = _gameServicesProvider.GetGamesByGenres(genres).ToList();
+                ViewBag.SelectedGenres = genres;
+            }
+            else
+            {
+                games = _gameServicesProvider.GetAllGames().ToList();
+                ViewBag.SelectedGenres = Enum.GetValues(typeof(Genre)).OfType<Genre>().ToList();
             }
 
             return View(new GamesViewModel { Games = games, TitleKey = titleKey });
