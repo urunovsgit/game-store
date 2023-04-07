cfc5a84b3c721c4443f31e39d869dc390480e18c
urunovsgit <urunovsbox@gmail.com>
2023-04-07 15:31:15 +0500

    Set Azure db connection string

--- game-store/Program.cs
+++ game-store/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddHealthChecks();
 
 builder.Services.AddDbContext<GameStoreDbContext>(opts =>
 {
-    opts.UseSqlServer(builder.Configuration["ConnectionStrings:GAMESTORE_DB_CONN_STR"]);
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:AZURE_SQL_CONNECTIONSTRING"]);
 });
 
 var app = builder.Build();
